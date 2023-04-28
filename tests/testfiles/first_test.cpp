531bd35420b2c413eccbd3c5285fdbcd47122b13
Krzysztof Para <k.para@contina.pl>
2023-04-29 00:21:56 +0200

    add automatic tests

--- tests/testfiles/first_test.cpp
+++ tests/testfiles/first_test.cpp
@@ -1,6 +1,7 @@
 #ifndef SOME_UNIQUE_NAME_HERE
 #define SOME_UNIQUE_NAME_HERE
 
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include "../doctest.h"
 
 TEST_CASE(("test") * doctest::test_suite("fisrt copy"))
