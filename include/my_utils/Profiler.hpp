edfde69950e9820eadf0401333d1a620184846db
Krzysztof Para <khris10@interia.pl>
2024-03-10 20:32:35 +0100

    format

--- include/my_utils/Profiler.hpp
+++ include/my_utils/Profiler.hpp
@@ -5,59 +5,52 @@
 #include <mutex>
 #include <string>
 #include <vector>
-struct Sample
-{
-    long nsTime;
-    std::string name;
-    size_t count = 1;
-    Sample() = default;
-    explicit Sample(std::string const &name)
-        : name(name)
-    {
-    }
-    Sample(std::string const &name, long nsTime)
-        : nsTime(nsTime), name(name)
-    {
-    }
+struct Sample {
+  long nsTime;
+  std::string name;
+  size_t count = 1;
+  Sample() = default;
+  explicit Sample(std::string const &name) : name(name) {}
+  Sample(std::string const &name, long nsTime) : nsTime(nsTime), name(name) {}
 };
 
-class Profiler
-{
+class Profiler {
 private:
-    std::mutex mxSamples;
-    std::vector<Sample> samples;
-    static Profiler *instance_;
-    ~Profiler();
-    Profiler();
+  std::mutex mxSamples;
+  std::vector<Sample> samples;
+  static Profiler *instance_;
+  ~Profiler();
+  Profiler();
 
 public:
-    static Profiler *getInstance();
-    Profiler(Profiler &other) = delete;
-    void operator=(const Profiler &) = delete;
+  static Profiler *getInstance();
+  Profiler(Profiler &other) = delete;
+  void operator=(const Profiler &) = delete;
 
-    void AddSample(Sample sample);
+  void AddSample(Sample sample);
 
-    std::string getTimingsAsString(bool doClearSamples = true);
-    std::vector<Sample> getTimings(bool doClearSamples = true);
+  std::string getTimingsAsString(bool doClearSamples = true);
+  std::vector<Sample> getTimings(bool doClearSamples = true);
 
-    void clearSamples();
-    void printProfilerData(bool doClearSamples = true);
+  void clearSamples();
+  void printProfilerData(bool doClearSamples = true);
 };
 
 #define TOKENPASTE(x, y) x##y
 #define TOKENPASTE2(x, y) TOKENPASTE(x, y)
-// use for acurate creation to block end timing cant be used in return scope //TODO deal with that problem
+// use for acurate creation to block end timing cant be used in return scope
+// //TODO deal with that problem
 #define newTimer(name) PTimer TOKENPASTE2(Timer_, __COUNTER__) = PTimer(name)
-// use by throwing newTimer({string name}) into code block, it will measure to the end of a block
-class PTimer
-{
+// use by throwing newTimer({string name}) into code block, it will measure to
+// the end of a block
+class PTimer {
 private:
-    Sample sample;
-    std::chrono::system_clock::time_point startTime;
+  Sample sample;
+  std::chrono::system_clock::time_point startTime;
 
 public:
-    explicit PTimer(const std::string &name);
-    ~PTimer();
+  explicit PTimer(const std::string &name);
+  ~PTimer();
 };
 
 #endif // PROFILER_HPP
--- main.cpp
+++ main.cpp
@@ -6,58 +6,57 @@
 #include <filesystem>
 #include <string>
 
-void prepareConfig(int argc, const char *argv[])
-{
-    std::cout << "test";
-    Config::parse(argc, argv);
-    logger.setLevel((Level)Config::get<Config::Argument::VERBOSE>());
-    if (Config::isUsed(Config::Argument::VERBOSE))
-    {
-        logger.orTarget(Target::STDOUT);
-    }
-    if (Config::isUsed(Config::Argument::LOG))
-    {
-        logger.setFile("./" + Config::get<Config::Argument::LOG>(), true);
-        logger.xorTarget(Target::LOG_FILE);
-    }
+void prepareConfig(int argc, const char *argv[]) {
+  std::cout << "test";
+  Config::parse(argc, argv);
+  logger.setLevel((Level)Config::get<Config::Argument::VERBOSE>());
+  if (Config::isUsed(Config::Argument::VERBOSE)) {
+    logger.orTarget(Target::STDOUT);
+  }
+  if (Config::isUsed(Config::Argument::LOG)) {
+    logger.setFile("./" + Config::get<Config::Argument::LOG>(), true);
+    logger.xorTarget(Target::LOG_FILE);
+  }
 }
 
-int main(int argc, const char *argv[])
-{
-    {
-        prepareConfig(argc, argv);
-        // file Exists
+int main(int argc, const char *argv[]) {
+  {
+    prepareConfig(argc, argv);
+    // file Exists
 
-        if (!std::filesystem::exists(Config::get<Config::Argument::INPUT>()))
-        {
-            logger.logErr("no input file: " + std::filesystem::absolute(Config::get<Config::Argument::INPUT>()).string());
-            return 2;
-        }
-        logger.logInfo("Starting");
-        ArtGeneration gen(Config::get<Config::Argument::POPULATION>(), Config::get<Config::Argument::SHAPE_AMOUNT>());
-        logger.logInfo("cairo_image_surface_create_from_png");
-        cairo_surface_t *image = cairo_image_surface_create_from_png(Config::get<Config::Argument::INPUT>().c_str());
-
-        int width = cairo_image_surface_get_width(image);
-        int height = cairo_image_surface_get_height(image);
-        unsigned char const *imgData = cairo_image_surface_get_data(image);
-
-        std::vector<unsigned char> flippedPixels(width * height * 4);
-        for (int y = 0; y < height; y++)
-        {
-            memcpy(flippedPixels.data() + (y * width * 4),
-                   imgData + ((height - y - 1) * width * 4),
-                   width * 4);
-        }
-        int stride = cairo_image_surface_get_stride(image);
-        cairo_surface_destroy(image);
-        image = cairo_image_surface_create_for_data(flippedPixels.data(), CAIRO_FORMAT_ARGB32, width, height, stride);
-
-        logger.logInfo("StartEvolution");
-        gen.startEvolution(image);
-        cairo_surface_destroy(image);
-        logger.logDeb(Profiler::getInstance()->getTimingsAsString().c_str());
+    if (!std::filesystem::exists(Config::get<Config::Argument::INPUT>())) {
+      logger.logErr(
+          "no input file: " +
+          std::filesystem::absolute(Config::get<Config::Argument::INPUT>())
+              .string());
+      return 2;
     }
+    logger.logInfo("Starting");
+    ArtGeneration gen(Config::get<Config::Argument::POPULATION>(),
+                      Config::get<Config::Argument::SHAPE_AMOUNT>());
+    logger.logInfo("cairo_image_surface_create_from_png");
+    cairo_surface_t *image = cairo_image_surface_create_from_png(
+        Config::get<Config::Argument::INPUT>().c_str());
 
-    return 0;
+    int width = cairo_image_surface_get_width(image);
+    int height = cairo_image_surface_get_height(image);
+    unsigned char const *imgData = cairo_image_surface_get_data(image);
+
+    std::vector<unsigned char> flippedPixels(width * height * 4);
+    for (int y = 0; y < height; y++) {
+      memcpy(flippedPixels.data() + (y * width * 4),
+             imgData + ((height - y - 1) * width * 4), width * 4);
+    }
+    int stride = cairo_image_surface_get_stride(image);
+    cairo_surface_destroy(image);
+    image = cairo_image_surface_create_for_data(
+        flippedPixels.data(), CAIRO_FORMAT_ARGB32, width, height, stride);
+
+    logger.logInfo("StartEvolution");
+    gen.startEvolution(image);
+    cairo_surface_destroy(image);
+    logger.logDeb(Profiler::getInstance()->getTimingsAsString().c_str());
+  }
+
+  return 0;
 }
